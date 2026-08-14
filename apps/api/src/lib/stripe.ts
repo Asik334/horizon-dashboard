382df023a17bd8a6e54ab3d9bd2762680f537fb8
Asik334 <asikesenalin0@gmail.com>
2026-08-14 09:15:03 +0600

    Fix API build

--- apps/api/src/lib/stripe.ts
+++ apps/api/src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { env } from "./env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-02-24.acacia",
 });
 
 interface CreateEntryCheckoutParams {
